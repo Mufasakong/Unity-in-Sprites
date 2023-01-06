12a6009a562e6a991ae09ea621ee361f5f2c2084
Mufasakong <Halipha.haidary@gmail.com>
2023-01-06 16:43:08 +0100

    Should now be fully operational

--- IKDSprites/Assets/Scripts/PlayerMovement.cs
+++ IKDSprites/Assets/Scripts/PlayerMovement.cs
@@ -2,55 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 movement;
+    private Transform myTransform;
     private Rigidbody2D myBody;
-    private Animator myAnimator;
+    private Animator myAnimation;
+    [SerializeField] private int speed = 1;
 
-    public BoxCollider2D myRope;
-
-    public int speed = 5;
+    float posX;
+    float posY;
+    
+    // public float jumpForce = 10f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-       myBody = GetComponent<Rigidbody2D>(); 
-       myAnimator = GetComponent<Animator>();
-       myRope = GetComponent<BoxCollider2D>();
+        myTransform = GetComponent<Transform>();
+        myBody = GetComponent<Rigidbody2D>();
+        myAnimation = GetComponent<Animator>();
     }
 
-    void OnMovement(InputValue value) {
+    void Update() {
+       Vector3 currentPos = myTransform.position;
+
+        // Set the object's position to the other side of the boundary if it has crossed either boundary on the x-axis or y-axis
+        currentPos.x = currentPos.x > 10 ? -10 : (currentPos.x < -10 ? 10 : currentPos.x);
+        currentPos.y = currentPos.y > 7 ? -7 : (currentPos.y < -7 ? 7 : currentPos.y);
+
+        myTransform.position = currentPos;
+    } 
+
+    // Update is called once per frame
+    private void OnMovement(InputValue value)
+    {
         movement = value.Get<Vector2>();
 
-        if (movement.x != 0 || movement.y != 0) {
-            myAnimator.SetFloat("x", movement.x);
-            //myAnimator.SetFloat("y", movement.y);
-
-            myAnimator.SetBool("isWalking", true);
-        } else {
-            myAnimator.SetBool("isWalking", false);
+        if (movement.x !=0 || movement.y !=0) {
+            myAnimation.SetFloat("x", movement.x);
+            myAnimation.SetFloat("y", movement.y);
+            myAnimation.SetBool("isWalking", true);
+        }else{
+            myAnimation.SetBool("isWalking", false);
         }
+
     }
-	void Update()
-	{
-        if (myBody.rotation < -88 || myBody.rotation > 89) {
-            myRope.isTrigger = true;
-        } else
-		{
-            myRope.isTrigger = false;
-        }
 
-        if (myBody.position.y <= -10)
-		{
-            SceneManager.LoadScene("SampleScene");
-        }
-	}
-	// Update is called once per frame
-	void FixedUpdate()
-    {
+    private void FixedUpdate(){
         myBody.velocity = movement * speed;
+        /*if (Input.GetKeyDown(KeyCode.Space)) {  
+        myBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }*/
     }
+
 }
